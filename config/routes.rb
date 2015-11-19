8dc3d16d95edbaf5ff439bea4bb48e2550317883
lauraevans99 <lauraevans99@gmail.com>
2015-11-18 17:00:50 -0800

    set up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # devise_for :users
+      devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+
 root to: 'events#index'
 
 resources :events do
