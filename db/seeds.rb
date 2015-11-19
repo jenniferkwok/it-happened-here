c3fcdc3ff1033d81fb5611b9fb2db34c76a8847b
Uriel <uriel.chavez@gmail.com>
2015-11-18 23:13:03 -0800

    password_digest changed to encrypted_password to match schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -86,187 +86,187 @@ Location.create(
 	name: 'Conservatory of Flowers',
 	address: '100 John F Kennedy Dr, San Francisco, CA 94118',
 	image: 'conservatoryofflowers.jpg',
-	place_id: 'ChIJ5abCmkWHhYARH3zgiLVc_Ew',
+	place_id: 'ChIJ5abCmkWHhYARH3zgiLVc_Ew'
 	)
 	Location.create( 
 	name: 'Flood Mansion',
 	address: '1000 California St, San Francisco, CA 94115',
 	image: 'floodmansion.jpg',
-	place_id: 'ChIJjaNJDsWAhYARpTzMWrvuiEI',
+	place_id: 'ChIJjaNJDsWAhYARpTzMWrvuiEI'
 	)
 	Location.create( 
 	name: "Lotta's Fountain",
 	address: 'Kearny St, San Francisco CA 94105',
 	image: 'lottafountain.jpg',
-	place_id: 'ChIJ-zVUQoiAhYAREEBQ1oqGsiE',
+	place_id: 'ChIJ-zVUQoiAhYAREEBQ1oqGsiE'
 	)
 	Location.create( 
 	name: 'California Historical Society',
 	address: '2090 Jackson St, San Francisco, CA 94105',
 	image: 'whittiermansion.jpg',
-	place_id: 'ChIJx2NZ-IeAhYARwOHJPBZJJBA',
+	place_id: 'ChIJx2NZ-IeAhYARwOHJPBZJJBA'
 	)
 	Location.create( 
 	name: 'War Memorial Opera House',
 	address: '301 Wan Ness Ave, San Francisco, CA 94109',
 	image: 'warmemorial.jpg',
-	place_id: 'ChIJ7UVGDpmAhYARBt5goMZ-AO0',
+	place_id: 'ChIJ7UVGDpmAhYARBt5goMZ-AO0'
 	)
 	Location.create( 
 	name: 'Palace of Fine Arts',
 	address: '3701 Lyon St, San Francisco, CA ',
 	image: 'palaceoffinearts.jpg',
-	place_id: 'ChIJRb6WFtWGhYARLN9EOlXR2JI',
+	place_id: 'ChIJRb6WFtWGhYARLN9EOlXR2JI'
 	)
 	Location.create( 
 	name: 'Orpheum Theatre',
 	address: '1192 Market St, San Francisco, CA 94102',
 	image: 'orpheumtheater.jpg',
-	place_id: 'ChIJ76ERUo6AhYARr8LiSU5hciU',
+	place_id: 'ChIJ76ERUo6AhYARr8LiSU5hciU'
 	)
 	
 	Location.create( 
 	name: 'Golden Gate Park',
 	address: '1232 John F Kennedy Dr, San Francisco, CA 94121',
 	image: 'goldengatepark.jpg',
-	place_id: 'ChIJY_dFYHKHhYARMKc772iLvnE',
+	place_id: 'ChIJY_dFYHKHhYARMKc772iLvnE'
 	)
 	Location.create( 
 	name: 'The Castro Theatre',
 	address: '429 Castro St, San Francisco, CA ',
 	image: 'castrotheater.jpg',
-	place_id: 'ChIJeWH4Txt-j4ARJZQ5pivU-hQ',
+	place_id: 'ChIJeWH4Txt-j4ARJZQ5pivU-hQ'
 	)
 	Location.create( 
 	name: 'San Francisco Armory',
 	address: '1800 Mission St, San Francisco, CA 94103',
 	image: 'armoryandarsenal.jpg',
-	place_id: 'ChIJOQnmcSF-j4ARqbgcqD6DI_k',
+	place_id: 'ChIJOQnmcSF-j4ARqbgcqD6DI_k'
 	)
 	Location.create( 
 	name: 'Grace Cathedral',
 	address: '1051 Taylor St, San Francisco, CA ',
 	image: 'gracecathedral.jpg',
-	place_id: 'ChIJY0dQvZKAhYARcB4643GOKCE',
+	place_id: 'ChIJY0dQvZKAhYARcB4643GOKCE'
 	)
 	Location.create( 
 	name: 'Beach Chalet',
 	address: '100 Great Highway, San Francisco, CA ',
 	image: 'beachchalet.jpg',
-	place_id: 'ChIJ1ShzE7mHhYARnIi1rffKeVc',
+	place_id: 'ChIJ1ShzE7mHhYARnIi1rffKeVc'
 	)
 	Location.create( 
 	name: 'Fairmont Hotel',
 	address: '950 Mason St, San Francisco, CA 94108',
 	image: 'fairmont.jpg',
-	place_id: 'ChIJN2S4EI2AhYAR9J4Qeh1U8Aw',
+	place_id: 'ChIJN2S4EI2AhYAR9J4Qeh1U8Aw'
 	)
 	Location.create( 
 	name: 'Spreckles Mansion',
 	address: '2080 Washington St, San Francisco, CA 94109',
 	image: 'sprecklesmansion.jpg',
-	place_id: 'ChIJTzY5cMGAhYARrhOKjfwFtp0',
+	place_id: 'ChIJTzY5cMGAhYARrhOKjfwFtp0'
 	)
 	Location.create( 
 	name: 'El Capitan Theater and Hotel',
 	address: '2353 Mission St, San Francisco, CA 94110',
 	image: 'elcapitantheater.jpg',
-	place_id: 'ChIJByNqcjx-j4ARADcHb-u2dX0',
+	place_id: 'ChIJByNqcjx-j4ARADcHb-u2dX0'
 	)
 	Location.create( 
 	name: 'Golden Gate Bridge',
 	address: 'Golden Gate Bridge, San Francisco CA 94129',
 	image: 'goldengate.jpg',
-	place_id: ' ChIJw____96GhYARCVVwg5cT7c0',
+	place_id: ' ChIJw____96GhYARCVVwg5cT7c0'
 	)
 	Location.create( 
 	name: 'Old US Mint',
 	address: '155 Hermann St, San Francisco, CA 94103',
 	image: 'oldusmint.jpg',
-	place_id: 'ChIJfw7KU6KAhYARb4UBE7BbCyc',
+	place_id: 'ChIJfw7KU6KAhYARb4UBE7BbCyc'
 	)
 	Location.create( 
 	name: 'The House from Full House',
 	address: 'Alamo Square, San Francisco, CA 94117',
 	image: 'alamosquare.jpg',
-	place_id: 'ChIJudgZRaWAhYARSP5xZKXpvV8',
+	place_id: 'ChIJudgZRaWAhYARSP5xZKXpvV8'
 	)
 
-	Users.create(
-		username: "JohnMason"
-		email: "jpmason@therock.com "
-		password_digest: "sean"
-		image: "seanconnery.jpg"
+	User.create(
+		username: "JohnMason",
+		email: "jpmason@therock.com ",
+		encrypted_password: "sean",
+		image: "seanconnery.jpg",
 		about: "Spent a lot of time looking at San Francisco from the best little window ever."
 		)
-	Users.create(
-		username: "Goodspeed1"
-		email: "stanley@fbi.gov"
-		password_digest: "nicolas"
-		image: "niccage.jpg"
+	User.create(
+		username: "Goodspeed1",
+		email: "stanley@fbi.gov",
+		encrypted_password: "nicolas",
+		image: "niccage.jpg",
 		about: "First visit and it was a gas.  Saw everything in a single drive."
 		)
-	Users.create(
-		username: "InspectorSteve"
-		email: "skeller@sfpd.gov"
-		password_digest: "michael"
-		image: "michaeldouglas.jpg"
+	User.create(
+		username: "InspectorSteve",
+		email: "skeller@sfpd.gov",
+		encrypted_password: "michael",
+		image: "michaeldouglas.jpg",
 		about: "The streets of San Francisco have always been paved with gold, or as we say, the hearts of its people."
 		)
-	Users.create(
-		username: "Reggie48"
-		email: "rhammond@penalsystem.org"
-		password_digest: "eddie"
-		image: "eddiemurphy.jpg"
+	User.create(
+		username: "Reggie48",
+		email: "rhammond@penalsystem.org",
+		encrypted_password: "eddie",
+		image: "eddiemurphy.jpg",
 		about: "If you have two days to spend in San Francisco, stay up for the nightlife."
 		)
-	Users.create(
-		username: "cadillac76"
-		email: "jcates@sfpd.gov"
-		password_digest: "nick"
-		image: "nicknolte.jpg"
+	User.create(
+		username: "cadillac76",
+		email: "jcates@sfpd.gov",
+		encrypted_password: "nick",
+		image: "nicknolte.jpg",
 		about: "Cable cars are a must.  So is a gas guzzling convertible with rust all over."
 		)
-	Users.create(
-		username: "antman2"
-		email: "slang@notacriminal.com"
-		password_digest: "paul"
-		image: "paulrudd.jpg"
+	User.create(
+		username: "antman2",
+		email: "slang@notacriminal.com",
+		encrypted_password: "paul",
+		image: "paulrudd.jpg",
 		about: "To truly enjoy San Francisco, you have to take time to admire all the small things."
 		)
-	Users.create(
-		username: "untidyharry"
-		email: "dirtyboy@sfpd.gov"
-		password_digest: "clint"
-		image: "clinteastwood.jpg"
+	User.create(
+		username: "untidyharry",
+		email: "dirtyboy@sfpd.gov",
+		encrypted_password: "clint",
+		image: "clinteastwood.jpg",
 		about: "Plan to enjoy everything, but don't overplan.  Really look at your plan and ask yourself if you feel lucky."
 		)
-	Users.create(
-		username: "amytan"
-		email: "ihatemymother@gmail.com"
-		password_digest: "amy"
-		image: "amytan.jpg"
+	User.create(
+		username: "amytan",
+		email: "ihatemymother@gmail.com",
+		encrypted_password: "amy",
+		image: "amytan.jpg",
 		about: "A true shopping adventure cannot happen without visiting Chinatown."
 		)
-	Users.create(
-		username: "englishnanny1"
-		email: "doubtfire@williams.com"
-		password_digest: "robin"
-		image: "robinwilliams.jpg"
+	User.create(
+		username: "englishnanny1",
+		email: "doubtfire@williams.com",
+		encrypted_password: "robin",
+		image: "robinwilliams.jpg",
 		about: "Watch out for the run by fruities."
 		)
-	Users.create(
-		username: "hateheights"
-		email: "sferguson@sfpd.com"
-		password_digest: "james"
-		image: "jamesstewart.jpg"
+	User.create(
+		username: "hateheights",
+		email: "sferguson@sfpd.com",
+		encrypted_password: "james",
+		image: "jamesstewart.jpg",
 		about: "Love the streets, the metro, the piers.  Not so much the rooftop views."
 		)
-	Users.create(
-		username: "zodiac22"
-		email: "incognito@sfchronicle.com"
-		password_digest: "jake"
-		image: "jakegylenhall.jpg"
+	User.create(
+		username: "zodiac22",
+		email: "incognito@sfchronicle.com",
+		encrypted_password: "jake",
+		image: "jakegylenhall.jpg",
 		about: "Any walk in any park is a great time to be had.  Try it at night."
 		)
 	
