cb8ba841d6dbc4699da43f42adc4a8eef8d76d83
lauraevans99 <lauraevans99@gmail.com>
2015-11-18 17:08:36 -0800

    cleaning up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # devise_for :users
-      devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
+devise_for :users, controllers: {
+   sessions: 'users/sessions'
+}
 
 root to: 'events#index'
 
