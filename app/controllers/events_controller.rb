4b04ad3230b75f1437e5cd37011d223a4a3701d4
lauraevans99 <lauraevans99@gmail.com>
2015-11-19 14:47:33 -0800

    geocoder is working

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,18 @@
 class EventsController < ApplicationController
+
+	# attr_accessor :geocoded_by
+
 	def index
 		@events = Event.all
 	end
 
 	def new
 		@event = Event.new
+		@location = Location.new
 	end
 
 	def create
-		event_params = params.require(:event).permit(:title, :year, :date, :link, :image, :content, :tag_art, :tag_people, :tag_science, :tag_politics, :tag_conflict, :tag_nature, :tag_sports)
+		event_params = params.require(:event).permit(:title, :year, :date, :link, :image, :content, :tag_art, :tag_people, :tag_science, :tag_politics, :tag_conflict, :tag_nature, :tag_sports, :location_id)
 		@event = Event.create(event_params)
 		redirect_to"/events"
 	end
