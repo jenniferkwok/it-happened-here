60e502f2197e98a70da69fae93c10873e72500a0
Uriel <uriel.chavez@gmail.com>
2015-11-23 13:01:07 -0800

    added locations and users to signed in nav bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,22 @@ class UsersController < ApplicationController
 
 	# before_action :authenticate_user!
 before_filter :authenticate_user!, :except => [:new]
+def index
+		@users = User.all
+# 		@events = Event.all
+# 		@locations = Location.all
+		
+		gon.users = @users
+# 		gon.locations = @locations
+# 		gon.quickEvents = @events
+		# redirect_to "/users"
 
+end
+
+def show
+	@user = User.all
+	# redirect_to "/users"
+end
 
 	# user_signed_in?
 	# current_user
