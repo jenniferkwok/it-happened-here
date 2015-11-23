46a527e1cc3e9486f4f80dab3723534710eb0019
Uriel <uriel.chavez@gmail.com>
2015-11-22 16:54:08 -0800

    heroku debugging

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
-  devise :omniauthable, omniauth_providers: [:twitter]
-def devise
-end
+
+ # devise :omniauthable, omniauth_providers: [:twitter]
+
+
   # You should also create an action method in this controller like this:
   def twitter
   end
